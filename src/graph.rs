3275def9cc834c811802f97a56fde64be0957bad
Steve Degosserie <steve@parity.io>
2019-11-23 01:31:02 +0100

    rename paint to palette, and modules to pallets

--- src/graph.rs
+++ src/graph.rs
@@ -9,36 +9,41 @@ use std::{
 };
 
 lazy_static! {
-    static ref PAINT: [String; 29] = [
-        "paint-assets".to_owned(),
-        "paint-aura".to_owned(),
-        "paint-authority-discovery".to_owned(),
-        "paint-authorship".to_owned(),
-        "paint-babe".to_owned(),
-        "paint-balances".to_owned(),
-        "paint-collective".to_owned(),
-        "paint-contracts".to_owned(),
-        "paint-council".to_owned(),
-        "paint-democracy".to_owned(),
-        "paint-elections".to_owned(),
-        "paint-example".to_owned(),
-        "paint-executive".to_owned(),
-        "paint-finality-tracker".to_owned(),
-        "paint-generic-asset".to_owned(),
-        "paint-grandpa".to_owned(),
-        "paint-im-online".to_owned(),
-        "paint-indices".to_owned(),
-        "paint-membership".to_owned(),
-        "paint-metadata".to_owned(),
-        "paint-offences".to_owned(),
-        "paint-scored-pool".to_owned(),
-        "paint-session".to_owned(),
-        "paint-staking".to_owned(),
-        "paint-sudo".to_owned(),
-        "paint-support".to_owned(),
-        "paint-system".to_owned(),
-        "paint-timestamp".to_owned(),
-        "paint-treasury".to_owned(),
+    static ref PALETTE: [String; 34] = [
+        "pallet-assets".to_owned(),
+        "pallet-aura".to_owned(),
+        "pallet-authority-discovery".to_owned(),
+        "pallet-authorship".to_owned(),
+        "pallet-babe".to_owned(),
+        "pallet-balances".to_owned(),
+        "pallet-collective".to_owned(),
+        "pallet-contracts".to_owned(),
+        "pallet-democracy".to_owned(),
+        "pallet-elections".to_owned(),
+        "pallet-elections-phragmen".to_owned(),
+        "pallet-evm".to_owned(),
+        "pallet-example".to_owned(),
+        "pallet-executive".to_owned(),
+        "pallet-finality-tracker".to_owned(),
+        "pallet-generic-asset".to_owned(),
+        "pallet-grandpa".to_owned(),
+        "pallet-im-online".to_owned(),
+        "pallet-indices".to_owned(),
+        "pallet-membership".to_owned(),
+        "pallet-metadata".to_owned(),
+        "pallet-nicks".to_owned(),
+        "pallet-offences".to_owned(),
+        "pallet-randomness-collective-flip".to_owned(),
+        "pallet-scored-pool".to_owned(),
+        "pallet-session".to_owned(),
+        "pallet-staking".to_owned(),
+        "pallet-sudo".to_owned(),
+        "pallet-support".to_owned(),
+        "pallet-system".to_owned(),
+        "pallet-timestamp".to_owned(),
+        "pallet-transaction-payment".to_owned(),
+        "pallet-treasury".to_owned(),
+        "pallet-utility".to_owned(),
     ];
 }
 
@@ -49,7 +54,7 @@ pub fn execute_graph(m: &ArgMatches) -> CliResult<()> {
     let manifest = read_manifest(&cfg.manifest_path)?;
 
     let mut filter = vec![manifest.package().as_ref().unwrap().name().to_owned()];
-    filter.append(&mut PAINT.to_vec());
+    filter.append(&mut PALETTE.to_vec());
     cfg.filter = Some(filter);
     cfg.transitive_deps = false;
 
